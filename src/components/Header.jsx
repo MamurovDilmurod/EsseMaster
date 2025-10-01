af2f378431c0667e08399c06c440be196af37fa1
Dilmurod Mamurov <mamurovd426@gmail.com>
2025-10-01 16:03:39 +0500

    Update assets, links, and text

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -19,7 +19,7 @@ const Header = () => {
 
                 {/* Title Mobile */}
                 <div className="md:hidden flex">
-                    <h1 className="text-2xl sm:text-3xl md:text-5xl font-extrabold text-gray-900 leading-tight md:leading-snug">
+                    <h1 className="text-2xl uppercase sm:text-3xl md:text-5xl font-extrabold text-gray-900 leading-tight md:leading-snug">
                         SAT matematikdan <span className="text-blue-600">700+ ball</span> olish va{" "}
                         <span className="block md:inline">Testlarni oson yechish usullari. </span>
                     </h1>
@@ -59,7 +59,7 @@ const Header = () => {
 
                     {/* Title */}
                     <div className="hidden md:flex">
-                        <h1 className="text-2xl sm:text-3xl md:text-5xl font-extrabold text-gray-900 leading-tight md:leading-snug">
+                        <h1 className="text-2xl uppercase sm:text-3xl md:text-5xl font-extrabold text-gray-900 leading-tight md:leading-snug">
                             SAT matematikdan <span className="text-blue-600">700+ ball</span> olish va{" "}
                             <span className="block md:inline">Testlarni oson yechish usullari. </span>
                         </h1>
@@ -70,20 +70,20 @@ const Header = () => {
                             href="https://t.me/+gpMBfd7Ka29iYWMy"
                             target="_blank"
                             rel="noopener noreferrer"
-                            className=" justify-center px-8 py-4 bg-blue-500 text-white text-base font-semibold rounded-lg 
+                            className=" justify-center px-8 py-5 bg-blue-500 text-white text-base font-semibold rounded-lg 
                        hover:bg-blue-600 transition-all duration-200 transform hover:scale-105 text-center md:hidden flex"
                         >
                             BEPUL QATNASHISH
                         </a>
                     </div>
                     {/* Subtitle */}
-                    <div className="!mt-[-25px]">
-                        <h2 className="text-xl sm:text-2xl font-semibold text-gray-800">
+                    <div className="!mt-[-20px]">
+                        <h2 className="text-xl sm:text-2xl font-bold text-gray-800">
                             Vebinarda siz bilan:
                         </h2>
 
                         {/* Benefits */}
-                        <ul className="space-y-4 text-gray-700 text-lg sm:text-xl">
+                        <ul className="space-y-4 text-gray-700 text-lg sm:text-xl font-semibold">
                             <li className="flex items-center gap-3">
                                 <FaCheckCircle className="text-blue-500 w-5 h-5 sm:w-6 sm:h-6 flex-shrink-0" />
                                 Qanday qilib ingliz tilini bilmasangiz ham 2 oyda SAT MATH’dan 700+ ball olish formulasi
@@ -116,9 +116,9 @@ const Header = () => {
                     {/* Bonus Block */}
                     <div className="flex items-center gap-3 bg-gray-100 p-4 rounded-2xl shadow-sm border border-gray-200">
                         <FaGift className="text-red-500 text-3xl" />
-                        <h3 className="text-gray-700 text-base sm:text-lg">
+                        <h3 className="text-gray-600 text-base sm:text-lg font-medium">
                             Hoziroq telegram kanalimga o'ting va{" "}
-                            <span className="font-semibold text-black">
+                            <span className=" text-black font-bold">
                                 10 ta BONUS mini darsimga
                             </span>{" "}
                             ega bo'ling
@@ -138,7 +138,7 @@ const Header = () => {
                     href="https://t.me/+gpMBfd7Ka29iYWMy"
                     target="_blank"
                     rel="noopener noreferrer"
-                    className=" justify-center px-8 py-4 bg-blue-500 text-white text-base font-semibold rounded-lg 
+                    className=" justify-center px-8 py-5 bg-blue-500 text-white text-base font-semibold rounded-lg 
                        hover:bg-blue-600 transition-all duration-200 transform hover:scale-105 text-center md:hidden flex"
                 >
                     BEPUL QATNASHISH
