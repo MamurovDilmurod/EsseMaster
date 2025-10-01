d7188d83d7d310e8a623e5aa1d8c27e944a67f9c
Dilmurod Mamurov <mamurovd426@gmail.com>
2025-10-01 16:25:55 +0500

    Update assets, links, and text,links

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,11 +4,11 @@ import image from "../assets/IMG_2738.png";
 
 const Header = () => {
     return (
-        <header className="w-full bg-white font-thin">
+        <header className="w-full bg-white font-sans">
             <div className="max-w-7xl mx-auto px-6 py-10 grid md:grid-cols-2 md:gap-12 gap-6 items-center">
                 {/* --- MOBIL TEPA BLOK: SANA & NARX BIR QATOR --- */}
                 <div className="md:hidden flex justify-center items-center gap-6 bg-white shadow-md p-2 rounded-xl mb-1 w-full">
-                    <p className="flex items-center gap-2 text-lg text-gray-700 font-medium">
+                    <p className="flex items-center gap-2 text-lg text-gray-700 font-bold">
                         <FaClock className="text-blue-600 font-semibold" />
                         7–8-oktyabr | 20:00
                     </p>
@@ -60,8 +60,8 @@ const Header = () => {
                     {/* Title */}
                     <div className="hidden md:flex">
                         <h1 className="text-2xl uppercase sm:text-3xl md:text-5xl font-extrabold text-gray-900 leading-tight md:leading-snug">
-                            SAT matematikdan <span className="text-blue-600">700+ ball</span> olish va{" "}
-                            <span className="block md:inline">Testlarni oson yechish usullari. </span>
+                            SAT matematikdan <span className="text-blue-600 font-bold">700+ ball</span> olish va{" "}
+                            <span className="block md:inline ">Testlarni oson yechish usullari. </span>
                         </h1>
                     </div>
                     {/* Mobileda btn */}
@@ -70,7 +70,7 @@ const Header = () => {
                             href="https://t.me/+gpMBfd7Ka29iYWMy"
                             target="_blank"
                             rel="noopener noreferrer"
-                            className=" justify-center px-8 py-5 bg-blue-500 text-white text-base font-semibold rounded-lg 
+                            className=" justify-center px-8 py-6 bg-blue-500 text-white text-base font-semibold rounded-lg 
                        hover:bg-blue-600 transition-all duration-200 transform hover:scale-105 text-center md:hidden flex"
                         >
                             BEPUL QATNASHISH
@@ -78,12 +78,12 @@ const Header = () => {
                     </div>
                     {/* Subtitle */}
                     <div className="!mt-[-20px]">
-                        <h2 className="text-xl sm:text-2xl font-bold text-gray-800">
+                        <h2 className="text-xl sm:text-2xl font-extrabold text-blak">
                             Vebinarda siz bilan:
                         </h2>
 
                         {/* Benefits */}
-                        <ul className="space-y-4 text-gray-700 text-lg sm:text-xl font-semibold">
+                        <ul className="space-y-4 text-gray-900 text-lg sm:text-xl font-semibold">
                             <li className="flex items-center gap-3">
                                 <FaCheckCircle className="text-blue-500 w-5 h-5 sm:w-6 sm:h-6 flex-shrink-0" />
                                 Qanday qilib ingliz tilini bilmasangiz ham 2 oyda SAT MATH’dan 700+ ball olish formulasi
@@ -138,7 +138,7 @@ const Header = () => {
                     href="https://t.me/+gpMBfd7Ka29iYWMy"
                     target="_blank"
                     rel="noopener noreferrer"
-                    className=" justify-center px-8 py-5 bg-blue-500 text-white text-base font-semibold rounded-lg 
+                    className=" justify-center px-8 py-6 bg-blue-500 text-white text-base font-semibold rounded-lg 
                        hover:bg-blue-600 transition-all duration-200 transform hover:scale-105 text-center md:hidden flex"
                 >
                     BEPUL QATNASHISH
